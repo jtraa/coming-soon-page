02997e32901ce57c65e6f049f15beee43b819d48
Jelle <jelletraa@gmail.com>
2019-05-07 01:04:50 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Coming Soon</title>
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link rel="stylesheet" href="resources/css/main.css">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.css.map" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css" rel="stylesheet">
+    <style>
+    html, body{
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+    }
+
+    #box{
+        height: 80px;
+        width: 400px;
+        background: #000;
+        margin-top: 18%;
+        margin-left: 33%;
+        font-size: 50px;
+        font-family: Nunito;
+        font-weight: Bold;
+        color: #5eb5bc;
+        text-align: center;
+        display: table-cell;
+        vertical-align: middle;
+        position: absolute;
+        border-radius: 2.5px;
+    }
+    </style>
+</head>
+<body>
+
+    <div style="position:absolute; z-index:99;" id="box" class=""> Coming Soon </div>
+
+    <div style="z-index: 1;" id="particles-js" class="leftinteractive interactive-bg column is-20">
+      </div>
+
+    <script src="resources/js/particles.js"></script>
+    <script src="resources/js/main.js"></script>
+</body>
+</html>
